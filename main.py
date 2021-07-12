9a3f0052a541edabb5e6f34e768f9b99eeda0aae
dochi2350 <65119486+dochi2350@users.noreply.github.com>
2021-07-12 21:39:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Key, Controller
 from macro import autoplay
 import time, json
 
-path = './test1.adofai'
+path = './youradofaifile.adofai'
 time.sleep(1)
 print("program started")
 autoplay(path)
